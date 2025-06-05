d9c247905dbf9757e0b953633db2eb1dfda4dd73
sai kumar <147718819+Saikumar1756841@users.noreply.github.com>
2025-06-05 11:37:28 +0530

    Update Login.js

--- src/Components/Authentication/Login.js
+++ src/Components/Authentication/Login.js
@@ -10,7 +10,8 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 
 
 
@@ -47,7 +48,7 @@ export default function SignIn() {
     }
   }, []);
   const BASE_URL = process.env.REACT_APP_API_BASE_URL;
-  const history = useHistory();
+  const navigate = useNavigate();
 
   // Redirect if token is available
   useEffect(() => {
@@ -56,7 +57,8 @@ export default function SignIn() {
     console.log("Token in useEffect:", token);
     if (token) {
       console.log("Redirecting to home because token is present...");
-      history.push("/");
+      navigate("/");
+
 
     }
   }, [token]);
